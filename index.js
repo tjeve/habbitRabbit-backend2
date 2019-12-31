f40c29cd197caf07b6715c0f7cb344c47a30bbf5
Terrence Eve <tijayeve@gmail.com>
2019-12-30 22:59:35 -0600

    added knex and postgres database

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const port = 3000
+const environment = process.env.NODE_ENV || 'development'
 
 app.get('/', (req, res) => res.send(
     'Hello World!'
     ))
 
+app.post('/users', (req, res) => res.send(
+    
+))
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
