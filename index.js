3789065876809e19b444c8878ee0af998c8e5c84
Terrence Eve <tijayeve@gmail.com>
2019-12-31 03:35:12 -0600

    wrote a user and habit route for server

--- index.js
+++ index.js
@@ -3,14 +3,48 @@ const express = require('express')
 const app = express()
 const port = 3000
 const environment = process.env.NODE_ENV || 'development'
+// For database
+const dbconfigs = require("./knexfile.js")
+const db = require('knex')(dbconfigs.development)
+const query = require("./queries.js")
 
+// ********************** SQL Queries **********************
+let prepJSON = (data) => {
+    return '<pre>' + JSON.stringify(data) + '</pre>'
+}
+
+let allHabitsForAUser = ``
+
+let getUsers = db('Users')
+                .then(function(users){
+                    let getName = (user) => { return user.name}
+                    let names = users.map(getName)
+                    console.log(names)
+                    return users
+                })
+
+let getHabits = db('Habits')
+                .then(function(habits) {
+                    // console.log(habits)
+                    return habits
+                })
+
+// ********************** Routes **********************
 app.get('/', (req, res) => res.send(
     'Hello World!'
-    ))
-
-app.post('/users', (req, res) => res.send(
-    
+))
+//--> Displays all Users
+app.get('/users', (req, res) => res.send(
+    '<pre>' + JSON.stringify(getUsers, null, 4) + '</pre>'
+))
+//--> Displays a Users list of Habits
+app.get('/habits', (req, res) => res.send(
+    '<pre>' + JSON.stringify(getHabits, null, 4) + '</pre>'
 ))
 
+//--> Adds a habit from a User to their list of Habits
+app.post('/addHabit', (req, res) => req.send(
+
+))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
