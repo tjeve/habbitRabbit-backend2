8ab8c9c2577023ae7066d6c72534f9cbff5224ae
Terrence Eve <tijayeve@gmail.com>
2020-01-02 21:24:20 -0600

    /Post route is now working and so is /add-new-habit route

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const fs = require('fs')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const environment = process.env.NODE_ENV || 'development'
-// For database
+// ========== Database Setup ==========
 const dbconfigs = require("./knexfile.js")
 const db = require('knex')(dbconfigs.development)
 const query = require("./queries.js")
+
+// ========== Setup Passport ==========
+const passport = require('passport')
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+
 
 // ********************** SQL Queries **********************
 let makeJSON = (data) => {
     return '<pre>' + JSON.stringify(data) + '</pre>'
 }
 
-function createHabit (userID, newHabit) {
+// function createHabit (userID, newHabit) {
+//     return db.raw(
+//         'INSERT INTO "Habits" ("user_id", "habit") Values (?, ?)',
+//         [userID, newHabit]
+//     )
+// }
+function createHabit (user) {
+    console.log("userID:", user.user_id)
+    console.log("newHabit:", user.habit)
     return db.raw(
         'INSERT INTO "Habits" ("user_id", "habit") Values (?, ?)',
-        [userID, newHabit]
+        [user.user_id, user.habit]
     )
 }
 
@@ -34,25 +54,23 @@ const getHabits = db('Habits')
                     return habits
                 })
 
+// UNCOMMENT WHEN YOU FIGURE OUT HOW TO QUERY
 const getUserHabitsQuery =
     `SELECT "Users"."id", "Users"."name","Users"."slug","Habits"."habit"
     FROM "Users" 
     JOIN "Habits" 
-    On "Users"."id" = "Habits"."user_id" 
+    On "Users"."id" = "Habits"."user_id"
+    WHERE "Users"."id" = ?
     order by "Users"."id"`
-const getUserHabits = db
-                .raw(getUserHabitsQuery)
-                .then(function(habits) {
-                    console.log(habits)
-                    return habits.rows
-                })
+const getUserHabits = (userId) => { //<-- Test in Postman: Choose Post, then click body, and enter an object, {"userand press sne
+   return  db
+            .raw(getUserHabitsQuery, [userId])
+            .then(function(habits) {
+                console.log(habits.rows)
+                return habits.rows
+            })
 
-// const getUserHabits = db('Habits')
-//                 .where({user_id: 7}) // <-- Will find all habits from User 7
-//                 .then(function(habits) {
-//                     console.log(habits)
-//                     return habits
-//                 })
+}
 
 let addHabitQuery = db('Habits')
                 .insert({
@@ -72,14 +90,35 @@ app.get('/habits', (req, res) => res.send(
     makeJSON(getHabits)
 ))
 //--> Displays all the habits for one user
-app.get('/user-habits', (req, res) => res.send(
-    getUserHabits
-))
+app.get('/user-habits/:userId', (req, res) => { // <-- You will eventually be able to remove the :userID once you get the Oauth working. 
+    getUserHabits(req.params.userId) // <-- Once Oauth is running you can use the User object you get back from facebook to find the userid. Might look something like "req.params.userId", console log it first
+        .then(function(result) {
+            console.log(result, "Line 103 -- /user-habits/:userId")
+            res.send(makeJSON(result))
+        })
+        .catch(function(error) {
+            res.status(404).send("Page Not Found")
+        })
+    
+    // getUserHabits
+    })
 
 //--> Adds a habit from a User to their list of Habits
-app.post('/add-new-habbit', (req, res) => req.send(
-    console.log(req.body)
-    // createHabit()
-))
+app.post('/add-new-habit', (req, res) => {
+    createHabit(req.body)
+    .then(function(result) {
+        res.send(result)
+    })
+    .catch(function(error) {
+        console.warn("Something's Wrong!", error)
+    })
+})
+
+app.post('/login', passport.authenticate('local', { 
+    successRedirect: '/', 
+    failureRedirect: '/login' 
+}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+require('./auth-facebook.js')(app, passport)
